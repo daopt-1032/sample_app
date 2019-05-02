0f4bdda1133850fd98c79b3e85521daca35d425b
oPhamThiDao <pham.thi.dao@sun-asterisk.com>
2019-05-02 17:13:44 +0700

    create users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    logger.error "RecordNotFound: #{exception.message}"
+    render_404
+  end
+
+  def hello
+    render html: "Hello word!"
+  end
+
+  def render_404
+    render file: "public/404.html", status: :not_found, layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "application#hello"
+
+  resources :users
+
+  match "*path" => "application#render_404", via: :all
 end
